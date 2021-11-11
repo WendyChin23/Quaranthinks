41a915cf55d542d4eb62ef159731affaa563aa49
WendyChin23 <kristinevelasquez23@gmail.com>
2021-11-11 19:58:15 +0800

    Update views.py

--- appdev/views.py
+++ appdev/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import(get_object_or_404,render,HttpResponseRedirect)
+from .forms import *
+from appdev.forms import AccountUserForm
 
 from appdev.models import AccountUser
 # Create your views here.
@@ -58,10 +63,89 @@ class Testimonial(View):
 
 class AccountDashboardView(View):
     def get(self, request):
-        account = AccountUser.objects.all()
+        accountuser = AccountUser.objects.all()
        
         context = {
-            'account' : AccountUser, #name that we want to use
+            'accountuser' : accountuser, #name that we want to use
             
         }
         return render(request,'Accountuser.html', context)
+
+    def post(self, request):
+        if request.method == 'POST':
+            if 'BtnUpdate' in request.POST:
+                print('update button clicked')
+                Idn = request.POST.get("idn-idn")                                                                                                                                                                                                                                                                                                                                            
+                fname = request.POST.get("first-name")
+                lname = request.POST.get("last-name")
+                Email = request.POST.get("email-email")				
+                Address = request.POST.get("address-address")
+                Age = request.POST.get("age-age")
+                Birthdate = request.POST.get("birth-date")
+                Username = request.POST.get("user-name")
+                Password = request.POST.get("pass-word")
+				
+                update_user = AccountUser.objects.filter(idn=Idn).update(firstname = fname, lastname = lname, address = Address,
+				email = Email, age = Age, birthdate = Birthdate, username = Username, password = Password )
+                print(update_user)
+                print('user updated')
+
+            elif 'BtnDelete' in request.POST:
+                print('delete button clicked')
+                Idn = request.POST.get("iidn-idn")
+                students = AccountUser.objects.filter(idn=Idn).delete()
+
+        return redirect('appdev:accountdashboard_view')
+
+
+class Signup(View):
+    # def get(self, request):
+    #     return render(request, 'student.html')
+
+    # def post(self, request):
+    #     form = StudentForm(request.POST)
+    #     if form.is_valid():
+    #         studentid = request.POST.get("course_id")
+    #         coursesname = request.POST.get("course_name")
+
+    #         form = Course(course_id = courseid, course_name = coursesname)
+    #         form.save()
+
+    #         return redirect('myapp1:dashboard_view')
+
+    #     else:
+    #         print(form.errors)
+    #         return HttpResponse('not valid')
+
+	def get(self, request):
+		return render(request, 'signup.html')
+
+	def post(self, request):		
+		form = AccountUserForm(request.POST)		
+		# fname = request.POST.get("firstname")
+		# print(fname)
+		# lname = request.POST.get("lastname")
+		# print(lname)
+		if form.is_valid():
+			# try:
+            
+			Idn = request.POST.get("idn")
+			fname = request.POST.get("firstname")
+			lname = request.POST.get("lastname")
+			Address = request.POST.get("address")
+			Age = request.POST.get("age")
+			Birthdate = request.POST.get("birthdate")
+			Username = request.POST.get("username")
+			Password = request.POST.get("password")
+			form = AccountUser(idn = Idn, firstname = fname, lastname = lname, address = Address, age = Age,
+			 birthdate = Birthdate, username = Username, password=Password)
+			form.save()	
+
+			#return HttpResponse('Student record saved!')			
+			return redirect('appdev:accountdashboard_view')
+			# except:
+			# 	raise Http404
+		else:
+			print(form.errors)
+			return HttpResponse('not valid')
+		

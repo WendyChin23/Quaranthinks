a3dda595facc0cd130b1013ec328e58a64cbb166
Psalm Alonzo <psalm.alonzo0723@gmail.com>
2021-11-15 17:18:42 +0800

    admin.py mo return og email ang object

--- appdev/models.py
+++ appdev/models.py
@@ -13,3 +13,6 @@ class AccountUser(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.email
+
